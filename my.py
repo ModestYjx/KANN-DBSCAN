a2d40fb88548064fcc574b877b90211d66fd850c
ModestYjx <929604665@qq.com>
2020-04-28 14:15:34 +0800

    使用plt散点图

--- my.py
+++ my.py
@@ -2,6 +2,8 @@ import math
 import copy
 import numpy as np
 from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+import random
 
 
 def loadDataSet(fileName, splitChar='\t'):
@@ -16,20 +18,21 @@ def loadDataSet(fileName, splitChar='\t'):
             curline = line.strip().split(splitChar)
             fltline = list(map(float, curline))
             dataSet.append(fltline)
+    # print(dataSet)
     return dataSet
 
 
-def dist(a,b):
+def dist(a, b):
     """
     用来计算两个样本点之间的距离
     :param a: 样本点
     :param b: 样本点
     :return: 两个样本点之间的距离
     """
-    return math.sqrt(math.pow(a[0]-b[0],2) + math.pow(a[1]-b[1],2))
+    return math.sqrt(math.pow(a[0] - b[0], 2) + math.pow(a[1] - b[1], 2))
 
 
-def returnDk(matrix,k):
+def returnDk(matrix, k):
     """
     用来计算第K最近的距离集合
     :param matrix: 距离矩阵
@@ -51,7 +54,7 @@ def returnDkAverage(Dk):
     sum = 0
     for i in range(len(Dk)):
         sum = sum + Dk[i]
-    return sum/len(Dk)
+    return sum / len(Dk)
 
 
 def CalculateDistMatrix(dataset):
@@ -78,14 +81,14 @@ def returnEpsCandidate(dataSet):
     for i in range(len(tmp_matrix)):
         tmp_matrix[i].sort()
     EpsCandidate = []
-    for k in range(1,len(dataSet)):
-        Dk = returnDk(tmp_matrix,k)
+    for k in range(1, len(dataSet)):
+        Dk = returnDk(tmp_matrix, k)
         DkAverage = returnDkAverage(Dk)
         EpsCandidate.append(DkAverage)
     return EpsCandidate
 
 
-def returnMinptsCandidate(DistMatrix,EpsCandidate):
+def returnMinptsCandidate(DistMatrix, EpsCandidate):
     """
     计算Minpts候选列表
     :param DistMatrix: 距离矩阵
@@ -100,42 +103,50 @@ def returnMinptsCandidate(DistMatrix,EpsCandidate):
             for j in range(len(DistMatrix[i])):
                 if DistMatrix[i][j] <= tmp_eps:
                     tmp_count = tmp_count + 1
-        MinptsCandidate.append(tmp_count/len(dataSet))
+        MinptsCandidate.append(tmp_count / len(dataSet))
     return MinptsCandidate
 
 
-def returnClusterNumberList(dataset,EpsCandidate,MinptsCandidate):
+def returnClusterNumberList(dataset, EpsCandidate, MinptsCandidate):
     """
-    计算聚类后的类别数目 
+    计算聚类后的类别数目
     :param dataset: 数据集
     :param EpsCandidate: Eps候选列表
     :param MinptsCandidate: Minpts候选列表
     :return: 聚类数量列表
     """
-    np_dataset = np.array(dataset)  #将dataset转换成numpy_array的形式
+    np_dataset = np.array(dataset)  # 将dataset转换成numpy_array的形式
     ClusterNumberList = []
     clusteringlabels_List = []
     for i in range(len(EpsCandidate)):
-        clustering = DBSCAN(eps= EpsCandidate[i],min_samples= MinptsCandidate[i]).fit(np_dataset)
+        clustering = DBSCAN(eps=EpsCandidate[i], min_samples=MinptsCandidate[i]).fit(np_dataset)
         num_clustering = max(clustering.labels_)
 
         clusteringlabels_List.append(clustering.labels_)
         ClusterNumberList.append(num_clustering)
 
-    #
-    print("clusteringlabels_List:")
-    print(clusteringlabels_List)
+    # print("clusteringlabels_List:")
+    # print(clusteringlabels_List)
     return ClusterNumberList
 
+
 if __name__ == '__main__':
-    dataSet = loadDataSet('788points.txt', splitChar=',')
+    dataSet = loadDataSet('./data/data7122/788points.txt', splitChar=',')
     EpsCandidate = returnEpsCandidate(dataSet)
     DistMatrix = CalculateDistMatrix(dataSet)
-    MinptsCandidate = returnMinptsCandidate(DistMatrix,EpsCandidate)
-    ClusterNumberList = returnClusterNumberList(dataSet,EpsCandidate,MinptsCandidate)
+    MinptsCandidate = returnMinptsCandidate(DistMatrix, EpsCandidate)
+    ClusterNumberList = returnClusterNumberList(dataSet, EpsCandidate, MinptsCandidate)
     # print("EpsCandidate:")
     # print(EpsCandidate)
     # print("MinptsCandidate:")
     # print(MinptsCandidate)
     # print('cluster number list is')
     # print(ClusterNumberList)
+
+    dataSet = np.array(dataSet)
+    # print(dataSet[:, 0])
+    # print(dataSet[:, 1])
+
+    num = len(dataSet)
+    plt.scatter(dataSet[:, 0], dataSet[:, 1], marker='o')
+    plt.show()
